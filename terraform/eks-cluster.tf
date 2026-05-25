7e47191e04c0761b557dae2d27529e6c6b25c706
John Molloy <johnnyboymolloy@gmail.com>
2026-05-25 18:42:27 +0200

    TF format

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -55,7 +55,7 @@ module "eks" {
   access_entries = {
     github_actions = {
       principal_arn = "arn:aws:iam::094508075399:role/GitHubActionsEKSRole"
-      
+
       policy_associations = {
         admin = {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
