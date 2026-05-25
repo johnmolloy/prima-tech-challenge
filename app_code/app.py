164087d7e6810f9c40573b58dc71fbdbfeb7afab
John Molloy <johnnyboymolloy@gmail.com>
2026-05-25 22:32:12 +0200

    Bandit picked up debug mode being on in app.py so we turn it off

--- app_code/app.py
+++ app_code/app.py
@@ -113,4 +113,4 @@ def create_user():
         return jsonify({"error": "Failed to process user creation"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
